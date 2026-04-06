afbb3ea9b017eab86f173181b800262b2358ae32
naamayahav <naamayaha@gmail.com>
2026-04-07 00:34:02 +0300

    hw

--- deepsort.py
+++ deepsort.py
@@ -1,7 +1,16 @@
-def deep_sorted(x:any)->str:
-    # Put your code here
-    pass
-
+def deep_sorted(x:any) -> str:
+    if type(x) == list:
+        return sorted([deep_sorted(i) for i in x], key=str)
+    elif type(x) == dict:
+        return {k: deep_sorted(v) for k, v in sorted(x.items())}
+    elif type(x) == set:
+        return {deep_sorted(i) for i in x}
+    elif type(x) == tuple:
+        return tuple(sorted([deep_sorted(i) for i in x], key=str))
+    else:
+        if type(x) == str and x.isdigit():
+            return int(x)
+        return x
 
 
 if __name__ == '__main__':
